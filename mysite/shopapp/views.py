bfd7e3898cf459121fa2c3504add70cfc6ce118e
bvb46 <bvb46@mail.ru>
2023-06-21 21:42:05 +0300

    shopapp_views.py

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+#
+def shop_index(request: HttpRequest):
+    print(request.path)
+    print(request.method)
+    print(request.headers)
+    return HttpResponse('<h1>Hellow World!</h1>')
